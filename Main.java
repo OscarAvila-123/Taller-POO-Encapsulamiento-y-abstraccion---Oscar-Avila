26c14cc9c9ffbffbb81ce3de09b8b13e85ae15a9
OscarAvila-123 <oscar.avila-n@uniminuto.edu.co>
2026-03-26 08:14:41 -0500

    Remove Main class and main method implementation

--- Main.java
+++ Main.java
@@ -1,19 +1,1 @@
-public class Main {
-    public static void main(String[] args) {
 
-        CuentaBancaria cuenta = new CuentaBancaria("12345", 1000);
-
-        System.out.println("====== CUENTA BANCARIA ======\n");
-        System.out.println("Número de cuenta: " + cuenta.getNumeroCuenta());
-        System.out.println("Saldo inicial: $" + cuenta.getSaldo());
-
-        System.out.println("\n--- Operaciones ---");
-
-        cuenta.depositar(500);
-        cuenta.retirar(200);
-        cuenta.retirar(2000); // prueba error
-
-        System.out.println("\nSaldo final: $" + cuenta.getSaldo());
-        System.out.println("\n=============================");
-    }
-}
